ce6cf8e8ce8a11a911197e473b75f08543727c28
bmagdab <mj.borysiak@student.uw.edu.pl>
2024-05-08 23:14:12 +0200

    adjustment for ud corpora

--- other_scripts/gum_spok.py
+++ other_scripts/gum_spok.py
@@ -1,16 +1,19 @@
 # conversation, vlog, interview
 import re
 
-with open('en_gum-ud-train.conllu', mode='r') as file:
+with open('en_gum-ud-test.conllu', mode='r') as file:
     source = file.read()
     heading = re.match('# global\.columns.*\\n', source)
-    source = source.removeprefix(heading.group())
-    spok = heading.group()
+    if heading:
+        source = source.removeprefix(heading.group())
+        spok = heading.group()
+    else:
+        spok = ''
     docs = source.split(sep='#  = # newdoc')
     for doc in docs:
         genre = re.match(' id = GUM_interview| id = GUM_vlog| id = GUM_conversation', doc)
         if genre:
             spok += '#' + doc
 
-with open('gum_spok_train.conllu', mode='w') as file:
+with open('gum_spok_test.conllu', mode='w') as file:
     file.write(spok)
